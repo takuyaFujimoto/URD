925e99cf482e33e36aee9330fb7d4590f5575204
takuyaFujimoto <takuya.fujimoto@i-vinci.co.jp>
2019-12-17 12:30:55 +0900

    勤務表の雛形データ作成スクリプト

--- script/README.md
+++ script/README.md
@@ -1,3 +1,7 @@
 # 開発で必要なスクリプトはこちらへ
 
-`pipenv run python3 src/作成したスクリプト.py`
+## ▼ 勤怠表の雛形を DB 作成するスクリプト
+
+1. setting.json に uid, year, month（ゼロ埋め）をセット
+2. key 情報（serviceAccountKey.json）を用意
+3. `pipenv run python3 src/createAttendance/do.py`
---
+++ script/src/createAttendance/do.py
@@ -0,0 +1,66 @@
+import calendar
+import requests
+import json
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# DAOの作成
+def create_dao():
+  cred = cred = credentials.Certificate(
+    "src/createAttendance/serviceAccountKey.json")
+  firebase_admin.initialize_app(cred)
+  db = firestore.client()
+  return db
+
+def get_holidays(year, month):
+  api_url = "https://holidays-jp.github.io/api/v1/{0}/date.json"
+  url = api_url.format(year)
+  response = requests.get(url)
+  holidays = json.loads(response.text)
+  yyyymm = "{0}-{1}-".format(year, month)
+  # 該当月の祝日のみ抽出
+  match_holidays = list(filter(lambda x: yyyymm in x, holidays.keys()))
+  if len(match_holidays) != 0:
+    return list(map(lambda x: x[len(yyyymm):], match_holidays))
+  else:
+    return []
+
+# 登録アイテムの作成
+def create_items(year, month, holidays):
+  # 曜日
+  week = ["Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat.", "Sun."]
+  week_num, end_of_month = calendar.monthrange(year, month)
+
+  result = []
+  for num in range(1, (end_of_month + 1)):
+    day = str(num) if 10 <= num else "0" + str(num)
+    is_holiday = "true" if day in holidays else "false"
+    result.append({
+      day: {
+        "break_time": "01:00",
+        "day_of_the_week": week[week_num],
+        "from": "09:00",
+        "to": "18:00",
+        "is_holiday": is_holiday
+      }
+    })
+    week_num = week_num + 1 if week_num < 6 else 0
+  return result
+
+# mainロジック
+if __name__ == "__main__":
+  f = open("src/createAttendance/setting.json", "r")
+  json_data = json.load(f)
+  uid = json_data["uid"]
+  year = json_data["year"]
+  month = json_data["month"]
+  holidays = get_holidays(year, month)
+  items = create_items(int(year), int(month), holidays)
+  dao = create_dao()
+  doc_ref = dao.collection("attendance").document(
+    uid).collection(year).document(month)
+  for item in items:
+    doc_ref.set(item, merge=True)
+  print("finish")
